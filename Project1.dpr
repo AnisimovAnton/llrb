81986425be4cb7d7ee17d6abf3c8eab8d0ffc75b
AnisimovAnton <qwweer1@yandex.ru>
2014-12-17 14:59:14 +0300

    done coding

--- Project1.dpr
+++ Project1.dpr
@@ -8,7 +8,38 @@ uses
 
 var
   llrb: TLeftLeaningRedBlackTree;
+  i: Integer;
+  lst: Integers;
 begin
   llrb := TLLRB.Create;
+  llrb.insert(5);
+  llrb.insert(4);
+  llrb.insert(8);
+  llrb.insert(1);
+  llrb.insert(3);
+  llrb.insert(2);
+  llrb.insert(2);
+  llrb.insert(5);
+  llrb.insert(9);
+  llrb.insert(0);
+  llrb.insert(4);
+  llrb.insert(6);
+  llrb.insert(7);
+  llrb.insert(8);
+  llrb.insert(5);
+  llrb.insert(5);
+
+
+  llrb.ListAll(lst);
+  for i := 0 to Length(lst)-1 do
+    write(lst[i],' ');
+  writeln('');
+  llrb.delete(4);
+  llrb.delete(7);
+  llrb.delete(5);
+  llrb.ListAll(lst);
+  for i := 0 to Length(lst)-1 do
+    write(lst[i],' ');
+  writeln('');
   llrb.Free;
 end.
--- UnLLRB.pas
+++ UnLLRB.pas
@@ -3,6 +3,8 @@ unit UnLLRB;
 interface
 
 type
+  Integers = array of Integer;
+
   PRBNode = ^TRBNode;
   TRBNode = record
     Left: PRBNode;
@@ -14,12 +16,16 @@ type
   TLLRB = class
   private
     root: PRBNode;
-//    procedure Insert(Node: PRBNode; key: Integer);
-//    procedure Insert(key: Integer);
+    FCount: Integer;
+    function insert(Parent: PRBNode; key: Integer): PRBNode; overload;
+    function deleteMin(Parent: PRBNode): PRBNode; overload;
+    function delete(Parent: PRBNode; key: Integer): PRBNode; overload;
   public
-    function Search(key: Integer): PRBNode;
-    procedure Insert(key: Integer); overload;
-    function Insert(Parent: PRBNode; key: Integer): PRBNode; overload;
+    function search(key: Integer): PRBNode;
+    procedure insert(key: Integer); overload;
+    procedure deleteMin; overload;
+    procedure delete(key: Integer); overload;
+    procedure ListAll(var lst: Integers);
   end;
 
   TLeftLeaningRedBlackTree = TLLRB;
@@ -66,7 +72,48 @@ begin
   Parent.Right.isRed := not Parent.Right.isRed;
 end;
 
-function TLLRB.Search(key: Integer): PRBNode;
+function moveRedLeft(Parent: PRBNode): PRBNode;
+begin
+  flipColors(Parent);
+  if isRed(Parent.Right.Left) then
+  begin
+    Parent.Right := rotateRight(Parent.Right);
+    Parent := rotateLeft(Parent);
+    flipColors(Parent);
+  end;
+  Result := Parent;
+end;
+
+function moveRedRight(Parent: PRBNode): PRBNode;
+begin
+  flipColors(Parent);
+  if isRed(Parent.Left.Left) then
+  begin
+    Parent := rotateRight(Parent);
+    flipColors(Parent);
+  end;
+  Result := Parent;
+end;
+
+function fixUp(Parent: PRBNode): PRBNode;
+begin
+  if isRed(Parent.Right) then
+    Parent := rotateLeft(Parent);
+  if isRed(Parent.Left) and Assigned(Parent.Left) and isRed(Parent.Left.Left) then
+    Parent := rotateRight(Parent);
+  if isRed(Parent.Left) and isRed(Parent.Right) then
+    flipColors(Parent);
+  Result := Parent;
+end;
+
+function min(Parent: PRBNode): PRBNode;
+begin
+  while Assigned(Parent.Left) do
+    Parent := Parent.Left;
+  Result := Parent;
+end;
+
+function TLLRB.search(key: Integer): PRBNode;
 begin
   Result := root;
   while Assigned(Result) do
@@ -79,31 +126,105 @@ begin
   end;
 end;
 
-procedure TLLRB.Insert(key: Integer);
+procedure TLLRB.insert(key: Integer);
 begin
   root := Insert(root, key);
   root.isRed := False;
 end;
 
-function TLLRB.Insert(Parent: PRBNode; key: Integer): PRBNode;
+function TLLRB.insert(Parent: PRBNode; key: Integer): PRBNode;
 begin
   if not Assigned(Parent) then
   begin
     Result := NewNode(key);
+    Inc(FCount);
     Exit;
   end;
-  if key = Parent.Key then Exit;
-  if key < Parent.Key then
+  if key = Parent.Key then Result := Parent
+  else if key < Parent.Key then
     Parent.Left := Insert(Parent.Left, key)
   else
     Parent.Right := Insert(Parent.Right, key);
-  if Parent.Right.isRed and not Parent.Left.isRed then
-    Parent := rotateLeft(Parent);
-  if Parent.Left.isRed and Parent.Left.Left.isRed then
-    Parent := rotateRight(Parent);
-  if Parent.Left.isRed and Parent.Right.isRed then
-    flipColors(Parent);
+  if isRed(Parent.Right) and not isRed(Parent.Left) then Parent := rotateLeft(Parent);
+  if isRed(Parent.Left) and isRed(Parent.Left.Left) then Parent := rotateRight(Parent);
+  if isRed(Parent.Left) and isRed(Parent.Right) then flipColors(Parent);
   Result := Parent;
+end;
+
+function TLLRB.deleteMin(Parent: PRBNode): PRBNode;
+begin
+  if not Assigned(Parent.Left) then
+  begin
+    Result := nil;
+    Dec(FCount);
+    Exit;
+  end;
+  if not isRed(Parent.Left) and not isRed(Parent.Left.Left) then
+    Parent := MoveRedLeft(Parent);
+  Parent.Left := deleteMin(Parent.Left);
+  Result := fixUp(Parent);
+end;
+
+procedure TLLRB.deleteMin;
+begin
+  root := DeleteMin(root);
+  root.isRed := False;
+end;
+
+procedure TLLRB.delete(key: Integer);
+begin
+  root := delete(root, key);
+  root.isRed := False;
+end;
+
+function TLLRB.delete(Parent: PRBNode; key: Integer): PRBNode;
+begin
+  if key < Parent.Key then
+  begin
+    if not isRed(Parent.Left) and not isRed(Parent.Left.Left) then
+      Parent := moveRedLeft(Parent);
+    Parent.Left := Delete(Parent.Left, key);
+  end
+  else
+  begin
+    if isRed(Parent.Left) then
+      Parent := rotateRight(Parent);
+    if (key = Parent.Key) and not Assigned(Parent.Right) then
+    begin
+      Result := nil;
+      Dec(FCount);
+      Exit;
+    end;
+    if not isRed(Parent.Right) and not isRed(Parent.Right.Left) then
+      Parent := moveRedRight(Parent);
+    if key = Parent.Key then
+    begin
+      Parent.Key := min(Parent.Right).Key;
+      Parent.Right := deleteMin(Parent.Right);
+    end
+    else
+      Parent.Right := delete(Parent.Right, key);
+  end;
+  Result := fixUp(Parent);
+end;
+
+
+procedure TLLRB.ListAll(var lst: Integers);
+var
+  i: Integer;
+  procedure ListKey(Parent: PRBNode);
+  begin
+    if Assigned(Parent.Left) then
+      ListKey(Parent.Left);
+    lst[i] := Parent.Key;
+    Inc(i);
+    if Assigned(Parent.Right) then
+      ListKey(Parent.Right);
+  end;
+begin
+  SetLength(lst, FCount);
+  i := 0;
+  ListKey(root);
 end;
 
 end.
